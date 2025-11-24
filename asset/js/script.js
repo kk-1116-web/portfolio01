2c72c5000648be7f2dfd26709fe3467a05caca6d
ELECS-NET\konishi-kanako <konishi-kanako@elecs-web.co.jp>
2025-11-24 11:04:48 +0900

    アニメーション実装

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,4 +1,19 @@
+
+// menuの閉開
+const menuButtons = document.querySelectorAll('.menu'); // 2つとも取得
+const menuInner = document.querySelector('.menu-inner');
+
+menuButtons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    menuInner.classList.toggle('is-visible');
+  });
+});
+
+// works
 document.addEventListener("DOMContentLoaded", () => {
+  
+
+
   const workContents = document.querySelectorAll(".work-content, .news-archive");
 
   const observer = new IntersectionObserver((entries) => {
@@ -11,7 +26,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
   workContents.forEach((item) => observer.observe(item));
 });
-workContents.forEach((item, i) => {
+workContents.forEach((item, i) => {//コンソールエラーが出ている
   item.style.setProperty("--index", i);
   observer.observe(item);
 });
+
